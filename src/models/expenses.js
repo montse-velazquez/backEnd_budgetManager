94f011410d33dc0be566c20a3abdf8cbbe75da9f
montse-velazquez <12337@coderacademy.edu.au>
2023-07-29 16:57:19 +1000

    Create Expenses Schema

---
+++ src/models/expenses.js
@@ -0,0 +1,27 @@
+const Expenses = [
+    {
+        category: "Eating Out",
+        amount: 0,
+    }, 
+    {
+        category: "Groceries",
+        amount: 10,
+    },
+    {
+        category: "Entertainment",
+        amount: 20,
+    },
+    {
+        category: "Bills",
+        amount: 30,
+    },
+
+] 
+
+module.exports = Expenses;
+// const expenseSchema = new Schema({
+//   category: { type: String, required: true },
+//   amount: { type: Number, required: true },
+//   date: { type: Date, required: true },
+//   description: { type: String, required: true },
+// });
