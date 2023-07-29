79f764fe21570a4bf6ba02f9d021803f44aaf0d9
montse-velazquez <12337@coderacademy.edu.au>
2023-07-29 16:45:37 +1000

    Add delete functions and routes

--- src/controllers/goals_controllers.js
+++ src/controllers/goals_controllers.js
@@ -60,5 +60,40 @@ const addAmount = (request, response) => {
         }
 }
 
+//Delete specific Goal by _id
+const deleteGoal = async(request, response) => {
+    //Proper DB syntax
+    // try {
+    //     const id = request.params.id;
+    //     const data = await Goal.findByIdAndDelete(id)
+    //     response.send(`Document with ${data.name} has been deleted..`)
+    // }
+    // catch (error) {
+    //     response.status(400).json({ message: error.message })
+    // }
 
-module.exports = {getGoals, addGoal, updateGoal}
+const id_goal = request.params.id;
+let indexToRemove = Goal.findIndex(obj => obj.id === id_goal);
+if (indexToRemove) {
+    Goal.splice(indexToRemove, 1)
+    response.send("Eliminated")
+}
+else {
+    response.status(404).send("Not found")
+}
+}
+
+// Delete all goals
+const deleteAllGoals = async(request, response) => {
+// proper DB syntax 
+// await Goal.deleteMany({})
+// response.json({
+//     "message": "All notes deleted"
+// })
+}
+
+
+module.exports = {getGoals, addGoal, updateGoal, deleteGoal, deleteAllGoals, addAmount}
+
+
+module.exports = {getGoals, addGoal, updateGoal, addAmount}
--- src/routes/goals_routes.js
+++ src/routes/goals_routes.js
@@ -1,12 +1,16 @@
 const express = require('express')
 const goalsRouter = express.Router();
-const {getGoals, addGoal,updateGoal} = require('../controllers/goals_controllers')
+const {getGoals, addGoal,updateGoal, addAmount, deleteGoal, deleteAllGoals} = require('../controllers/goals_controllers')
 
 goalsRouter.get("/", getGoals)
 
 goalsRouter.post("/", addGoal)
 
 goalsRouter.put("/:id", updateGoal)
+
+goalsRouter.delete("/:id", deleteGoal)
+
+goalsRouter.delete("/delete", deleteAllGoals)
 
 goalsRouter.put("/addAmount/:goalId", addAmount)
 
