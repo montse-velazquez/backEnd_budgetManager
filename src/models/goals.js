464cd82b9c3bc829429ace14f6245a59f0c6f564
montse-velazquez <12337@coderacademy.edu.au>
2023-07-29 16:28:02 +1000

    Add Goal Model

---
+++ src/models/goals.js
@@ -0,0 +1,24 @@
+const Goal = [
+    {
+        id: 0,
+        title: "Tattoo's",
+        targetAmount: 2000,
+        initialAmount: 200
+    }, 
+    {
+        id: 1,
+        title: "Travel",
+        targetAmount: 1000,
+        initialAmount: 200
+    }
+]
+
+// export const GoalSchema = mongoose.Schema({
+//     title: String,
+//     targetAmount: Number,
+//     initialAmount: Number
+// });
+
+// const Goal = mongoose.model('Goal', GoalSchema)
+
+module.exports = Goal;
